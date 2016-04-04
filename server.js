c7790838c379de6795e705f1c744082251520720
Nathan L <adamnate@gmail.com>
2016-04-04 09:49:35 -0700

    working

--- server.js
+++ server.js
@@ -6,7 +6,11 @@ var express = require('express'),
 // and populate the req.body object
 var bodyParser = require('body-parser');
 app.use(bodyParser.urlencoded({ extended: true }));
-
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
 /************
  * DATABASE *
  ************/
